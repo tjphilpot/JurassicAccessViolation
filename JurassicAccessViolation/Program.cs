93a20820710fca48dd4e3610eda1b2826b6e9656
Tom Philpot <tom.philpot@logos.com>
2012-11-20 16:28:24 -0700

    Recreating as a VS2010 project

---
+++ JurassicAccessViolation/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using Jurassic;
+
+namespace JurassicAccessViolationTest
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string xmlDomJs = File.ReadAllText("xmldom.js");
+			string loadAbbrevsJs = File.ReadAllText("loadabbrevs.js");
+			string citeprocJs = File.ReadAllText("citeproc.js");
+
+			for (int i = 0; i < 100; i++)
+			{
+				Console.WriteLine("Loading into engine - attempt {0}", i);
+				ScriptEngine engine = new ScriptEngine();
+				engine.Execute(xmlDomJs);
+				engine.Execute(loadAbbrevsJs);
+				engine.Execute(citeprocJs);
+			}
+		}
+	}
+}
